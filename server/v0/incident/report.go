e366a02fa49511b14982cba54bd884bb1d9857de
delta <delta@deltalab.dev>
2024-07-20 02:19:22 +0900

    🍻 misc: added debug

--- server/v0/incident/report.go
+++ server/v0/incident/report.go
@@ -172,7 +172,7 @@ func (i *Incident) Query(ctx *fiber.Ctx) error {
 		qb = qb.Where(incident.ID(*req.IncidentID))
 	} else {
 		qb = qb.Where(func(selector *sql.Selector) {
-			selector.Where(sql.ExprP("ST_DWithin(location, 'POINT(?, ?), ?)", req.Longitude, req.Latitude, req.Radius))
+			selector.Where(sql.ExprP("ST_DWithin(location, 'POINT(?, ?)', ?)", req.Longitude, req.Latitude, req.Radius))
 		})
 	}
 
